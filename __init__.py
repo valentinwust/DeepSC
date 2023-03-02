a7ff848550db725217e33baad63890f42e0463c2
valentinwust <44555270+valentinwust@users.noreply.github.com>
2023-03-02 14:31:49 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-import .model
-import .module
-import .nn
-import .plot
-import .shap
-import .util
+import .model as model
+import .module as module
+import .nn as nn
+import .plot as plot
+import .shap as shap
+import .util as util
